4ef94d050e5fa932ba9de64ce061f60aede436f9
EdoSb111 <edo.sabri123@gmail.com>
2024-03-06 22:22:49 +0200

    Add post filtering functionality

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,9 +6,12 @@ import { getPosts } from '../../utils/posts-service';
 import CreatePostButton from '../CreatePost/CreatePostButton/CreatePostButton';
 import { FaPlus } from 'react-icons/fa';
 import { SiRocketdotchat } from 'react-icons/si';
+import Switch from '@mui/material/Switch';
 
 const Home = () => {
     const [posts, setPosts] = useState<PostProps[]>([]);
+    const [filteredPosts, setFilteredPosts] = useState<PostProps[]>([]);
+    const [onlyMyPosts, setOnlyMyPosts] = useState(false);
     useEffect(() => {
         (async () => {
             const posts = await getPosts();
@@ -16,18 +19,45 @@ const Home = () => {
         })();
     }, []);
 
-    const renderPosts = posts.map((post) => {
+    useEffect(() => {
+        setFilteredPosts(
+            onlyMyPosts
+                ? posts.filter(
+                      (post) =>
+                          post.createdBy._id === localStorage.getItem('userId')
+                  )
+                : posts
+        );
+    }, [onlyMyPosts, posts]);
+
+    const renderPosts = filteredPosts.map((post) => {
         return <Post {...post} key={post.id} />;
     });
 
     return (
         <div className={styles.home}>
+            {localStorage.getItem('userId') ? (
+                <div className={styles.toggleWrapper}>
+                    <Switch
+                        onChange={() => setOnlyMyPosts((checked) => !checked)}
+                    />
+                    Only show my posts
+                </div>
+            ) : null}
             <div className={styles.chatButtonWrapper}>
-                <CreatePostButton linkTo="/chat" tooltipText='Go to chat' icon={SiRocketdotchat} />
+                <CreatePostButton
+                    linkTo="/chat"
+                    tooltipText="Go to chat"
+                    icon={SiRocketdotchat}
+                />
             </div>
             <div className={styles.postsWrapper}>{renderPosts}</div>
             <div className={styles.createPostWrapper}>
-                <CreatePostButton linkTo="/createPost" tooltipText='Create a new post' icon={FaPlus} />
+                <CreatePostButton
+                    linkTo="/createPost"
+                    tooltipText="Create a new post"
+                    icon={FaPlus}
+                />
             </div>
         </div>
     );
