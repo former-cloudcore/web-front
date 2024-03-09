c1c0dc4eacedb6c79093496b3d444cc27e7f01f5
EdoSb111 <edo.sabri123@gmail.com>
2024-03-09 20:44:13 +0200

    Delete unnecessary assets and files

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    // server: {
-    //     https: true,
-    // },
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    if (mode === 'development') {
+        return {
+            plugins: [react()],
+        };
+    }
+
+    if (mode === 'production') {
+        return {
+            server: {
+                https: true,
+            },
+            plugins: [react(), mkcert()],
+        };
+    }
+
+    return {
+        plugins: [react()],
+    };
 });
