318d1a0a4defbe3f36ded47e2a67c6c00c3b9fab
EdoSb111 <edo.sabri123@gmail.com>
2024-03-09 19:34:54 +0200

    Add vite-plugin-mkcert and update SERVER_URL

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'http://localhost:3000';
+export const SERVER_URL = import.meta.env.MODE === 'development'?'http://localhost:3000':'https://node26.cs.colman.ac.il:4000';
 
 export const DEFAULT_IMAGE = 'https://media0.giphy.com/media/hryis7A55UXZNCUTNA/giphy.gif';
