e04e09cb20b7a72b9d4f7a486a2dc5e353edc907
EdoSb111 <edo.sabri123@gmail.com>
2024-03-12 22:22:00 +0200

    Add scrollbar styles to Home and Post components

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = import.meta.env.MODE === 'development'?'http://localhost:3000/api':'https://node26.cs.colman.ac.il:4000/api';
+export const SERVER_URL = import.meta.env.MODE === 'development'?'http://localhost:3000/api':'/api';
 
 export const DEFAULT_IMAGE = 'https://media0.giphy.com/media/hryis7A55UXZNCUTNA/giphy.gif';
