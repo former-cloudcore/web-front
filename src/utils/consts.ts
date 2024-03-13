7c42c34e30b913eef9853d62c2ef63c5bd19cbea
jofjof <116435248+jofjof@users.noreply.github.com>
2024-03-13 15:04:53 +0200

    remove / might work

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL = import.meta.env.MODE === 'development'?'http://localhost:3000/api':'/api';
-export const SOCKET_URL = import.meta.env.MODE === 'development'?'http://localhost:3000':'https://node26.cs.colman.ac.il/';
+export const SOCKET_URL = import.meta.env.MODE === 'development'?'http://localhost:3000':'https://node26.cs.colman.ac.il';
 
 export const DEFAULT_IMAGE = 'https://media0.giphy.com/media/hryis7A55UXZNCUTNA/giphy.gif';
